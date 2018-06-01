0661558f39a513877186afa2f93362cd2f351fa3
Chia-Ming Chang <skypc785308@gmail.com>
2018-06-01 10:32:31 +0800

    2018.06.01 簽口登記付款時自動塞選名稱是簽口的帳簿

--- uw_custom/sign_check/models/account_invoice.py
+++ uw_custom/sign_check/models/account_invoice.py
@@ -15,39 +15,15 @@ class AccountInvoiceSignCheck(models.Model):
         if len(order):
             self.sign_pay = True
 
-    # def action_invoice_open(self):
-    #     res = super(AccountInvoiceSignCheck, self).action_invoice_open()
-    #     sign = self.env['sign.main']
-    #     if self.sign_check is True:
-    #         partner = sign.search([('partner_id', '=', self.partner_id.id)])
-    #         if len(partner) > 0:
-    #             partner.write({
-    #                 'sign_account': [(0, 0, {
-    #                     'invoice_from': self.id,
-    #                     'price': self.amount_total
-    #                 })]
-    #             })
-    #         else:
-    #             sign.create({
-    #                 'partner_id': self.partner_id.id,
-    #                 'sign_account': [(0, 0, {
-    #                     'invoice_from': self.id,
-    #                     'price': self.amount_total
-    #                 })]
-    #             })
-    #
-    #     if self.sign_pay is True:
-    #         partner = sign.search([('partner_id', '=', self.partner_id.id)])
-    #         if len(partner) > 0:
-    #             less = partner.last_total
-    #             if less - self.amount_total < 0:
-    #                 raise UserError("錯誤!用戶 %s 剩餘簽口不足付完全額，差額為 %s" % (self.partner_id.name, less))
-    #             partner.write({
-    #                 'sign_account': [(0, 0, {
-    #                     'invoice_from': self.id,
-    #                     'price': -self.amount_total
-    #                 })]
-    #             })
-    #         else:
-    #             raise UserError("錯誤!用戶 %s 沒有簽口紀錄" % self.partner_id.name)
-    #     return res
+
+class AccountPaymentSignCheck(models.Model):
+    _inherit = 'account.payment'
+
+    @api.onchange('journal_id')
+    def set_default_iournal_id(self):
+        if self.invoice_ids:
+            for line in self.invoice_ids:
+                if line.sign_pay is True:
+                    sign_book = self.env['account.journal'].search([('name', 'like', '簽口')])
+                    self.journal_id = sign_book.id
+                    return {'domain': {'journal_id': [('name', 'like', '簽口')]}}
